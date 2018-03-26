903846b2926987de592325ef2e84edfc6111471c
MazilaFx <lakimore@mail.ru>
2018-03-26 23:53:27 +0300

    initial commitdfdfdfd

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,14 @@
 var gulp = require('gulp'),
-	sass = require('gulp-sass');
-
+	sass = require('gulp-sass'),
+	autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('sass', function(){
 	return gulp.src(['sass/**/*.sass', 'sass/**/*.scss'])
 	.pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+	.pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
 	.pipe(gulp.dest('css'))
 });
 
