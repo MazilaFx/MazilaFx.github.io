2e9a32042de0cda19bac41603e1f11f1ef800b20
MazilaFx <lakimore@mail.ru>
2018-03-27 22:40:09 +0300

    init

--- js/script.js
+++ js/script.js
@@ -17,61 +17,31 @@ $(document).ready(function(){
 });
 
 
-$(document).ready(function(){
-	$('#photo').click(function(){
-		var opt = $('#photo_1').attr('src', 'img/photography_1.jpg');
-		var opt = $('#photo_2').attr('src', 'img/photography_2.jpg');
-		var opt = $('#photo_3').attr('src', 'img/photography_3.jpg');
-		var opt = $('#photo_4').attr('src', 'img/photography_4.jpg');
-		var opt = $('#photo_5').attr('src', 'img/photography_5.jpg');
-		var opt = $('#photo_6').attr('src', 'img/photography_6.jpg');
-	});
-});
-
-
-
-
-$(document).ready(function(){
-	$('#web_design').click(function(){
-		var opt = $('#photo_1').attr('src', 'img/web_design_1.jpg');
-		var opt = $('#photo_2').attr('src', 'img/web_design_2.jpg');
-		var opt = $('#photo_3').attr('src', 'img/web_design_3.jpg');
-		var opt = $('#photo_4').attr('src', 'img/web_design_4.jpg');
-		var opt = $('#photo_5').attr('src', 'img/web_design_5.jpg');
-		var opt = $('#photo_6').attr('src', 'img/web_design_6.jpg');
-	});
-});
-
-
-$(document).ready(function(){
-	$('#branding').click(function(){
-		var opt = $('#photo_1').attr('src', 'img/branding_1.jpg');
-		var opt = $('#photo_2').attr('src', 'img/branding_2.jpg');
-		var opt = $('#photo_3').attr('src', 'img/branding_3.jpg');
-		var opt = $('#photo_4').attr('src', 'img/branding_4.jpg');
-		var opt = $('#photo_5').attr('src', 'img/branding_5.jpg');
-		var opt = $('#photo_6').attr('src', 'img/branding_6.jpg');
-	});
-});
-
-
-
-
-$(document).ready(function(){
-	$('#mobile_app').click(function(){
-		var opt = $('#photo_1').attr('src', 'img/mobile_app_1.jpg');
-		var opt = $('#photo_2').attr('src', 'img/mobile_app_2.jpg');
-		var opt = $('#photo_3').attr('src', 'img/mobile_app_3.jpg');
-		var opt = $('#photo_4').attr('src', 'img/mobile_app_4.jpg');
-		var opt = $('#photo_5').attr('src', 'img/mobile_app_5.jpg');
-		var opt = $('#photo_6').attr('src', 'img/mobile_app_6.jpg');
-	});
-});
-
-
-
-
-
-
-
-
+(function (jQuery) {
+ jQuery.fn.lightTabs = function (options) {
+  var createTabs = function () {
+   var tabs = this;
+   var i = 0;
+   showPage = function (i,tabs) {         
+     $(tabs).children("div").children("div").hide();
+     $(tabs).children("div").children("div").eq(i).fadeIn("900");
+     $(tabs).children("ul").children("li").removeClass("active-tab");
+     $(tabs).children("ul").children("li").eq(i).addClass("active-tab");
+   }
+   showPage(0,tabs);
+ 	
+  jQuery(tabs).children("ul").children("li").each(function (index, element) {
+     $(element).attr("data-page", i);
+     i++;
+  });
+  jQuery(tabs).children("ul").children("li").click(function () {
+        var tabs1 = $(this).parent().parent();
+        showPage(parseInt($(this).attr("data-page")),tabs1);
+   });
+ };
+ return this.each(createTabs);
+};
+})(jQuery);
+  
+ jQuery(".tabs").lightTabs(); 
+ 
